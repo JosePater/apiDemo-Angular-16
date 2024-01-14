f6bc77cb9a7dcc40c124919191703b645458f1e3
PaterDev <josepater_1130@hotmail.com>
2024-01-13 22:50:52 -0500

    feat(api): consumed api

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../service/api.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
 
+  // Con la urlApi se obtiene un objeto
+  data: any = {};
+
+  constructor(private apiService: ApiService) {}
+
+  ngOnInit(): void {
+    // llamar al método del servicio de la api
+    this.callServiceApi();
+  }
+
+  // llamar al servicio de la api
+  callServiceApi() {
+    this.apiService.getData().subscribe((data) => {
+      this.data = data;
+      console.log(this.data);
+    });
+  }
 }
